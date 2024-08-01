d67978efd3d91c71f46215bbae5045bc54b7851b
jamesnicolas26 <58098815+jamesnicolas26@users.noreply.github.com>
2024-08-01 13:41:48 +0800

    Add files via upload

---
+++ App.py
@@ -0,0 +1,43 @@
+# Basic Tic-Tac-Toe
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+def check_winner(board, player):
+    win_conditions = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+    ]
+    return [player, player, player] in win_conditions
+
+def tic_tac_toe():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    for _ in range(9):
+        print_board(board)
+        row = int(input(f"Player {current_player}, enter row (0-2): "))
+        col = int(input(f"Player {current_player}, enter column (0-2): "))
+
+        if board[row][col] == " ":
+            board[row][col] = current_player
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} won!")
+                return
+            current_player = "O" if current_player == "X" else "X"
+        else:
+            print("Invalid move! Try again.")
+
+    print_board(board)
+    print("It's a tie!")
+
+tic_tac_toe()
